ed8ad8da307fdce891d947d2c58621061288fb33
jlbuild <juliabuildbot@gmail.com>
2020-10-16 19:01:51 +0000

    Xorg_libXtst_jll build 1.2.3+4

--- src/wrappers/i686-linux-gnu.jl
+++ src/wrappers/i686-linux-gnu.jl
@@ -4,52 +4,15 @@ export libXtst
 using Xorg_libXext_jll
 using Xorg_libXfixes_jll
 using Xorg_libXi_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libXtst`
-const libXtst_splitpath = ["lib", "libXtst.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libXtst_path = ""
-
-# libXtst-specific global declaration
-# This will be filled out by __init__()
-libXtst_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libXtst = "libXtst.so.6"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Xorg_libXtst")
+JLLWrappers.@declare_library_product(libXtst, "libXtst.so.6")
 function __init__()
-    global artifact_dir = abspath(artifact"Xorg_libXtst")
+    JLLWrappers.@generate_init_header(Xorg_libXext_jll, Xorg_libXfixes_jll, Xorg_libXi_jll)
+    JLLWrappers.@init_library_product(
+        libXtst,
+        "lib/libXtst.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Xorg_libXext_jll.PATH_list, Xorg_libXfixes_jll.PATH_list, Xorg_libXi_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Xorg_libXext_jll.LIBPATH_list, Xorg_libXfixes_jll.LIBPATH_list, Xorg_libXi_jll.LIBPATH_list,))
-
-    global libXtst_path = normpath(joinpath(artifact_dir, libXtst_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libXtst_handle = dlopen(libXtst_path)
-    push!(LIBPATH_list, dirname(libXtst_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
